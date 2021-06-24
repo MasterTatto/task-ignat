6a36a4af0b0cdb3895abd5ca367fbb32e3474019
MasterTatto <gromvolk97@gmail.com>
2021-06-24 21:53:22 +0300

    finish lesson 2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import s from './Affairs.module.css';
+import { AffairType } from './HW2';
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
-}
+	// key не нужно типизировать
+	affair: AffairType; // need to fix any
+	deleteAffairCallback: (id: number) => void; // need to fix any
+};
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+	const deleteCallback = () => {
+		props.deleteAffairCallback(props.affair._id);
+	};
+	//
+	return (
+		<div className={s.box}>
+			<span
+				className={`${s.text} ${props.affair.priority === 'high' ? s.h : ''} ${
+					props.affair.priority === 'low' ? s.l : ''
+				} ${props.affair.priority === 'middle' ? s.m : ''}`}
+			>
+				{props.affair.priority}
+			</span>
 
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+			<button onClick={deleteCallback} className={s.remove}>
+				X
+			</button>
+		</div>
+	);
 }
 
-export default Affair
+export default Affair;
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,61 @@
-import React from 'react'
-import Affair from './Affair'
-import {AffairType} from './HW2'
-
-type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
-}
+import React from 'react';
+import Affair from './Affair';
+import { AffairType, FilterType } from './HW2';
+import s from './Affairs.module.css';
+type AffairsPropsType = {
+	// need to fix any
+	data: Array<AffairType>;
+	setFilter: (f: FilterType) => void;
+	deleteAffairCallback: (id: number) => void;
+	filter: FilterType;
+};
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+	const mappedAffairs = props.data.map((a: AffairType) => (
+		<Affair // should work
+			key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+			affair={a}
+			deleteAffairCallback={props.deleteAffairCallback}
+		/>
+	));
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+	const setAll = () => {
+		props.setFilter('all');
+	}; // need to fix
+	const setHigh = () => {
+		props.setFilter('high');
+	};
+	const setMiddle = () => {
+		props.setFilter('middle');
+	};
+	const setLow = () => {
+		props.setFilter('low');
+	};
 
-    return (
-        <div>
+	return (
+		<div className={s.owner}>
+			{mappedAffairs}
 
-            {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+			<button onClick={setAll} className={props.filter === 'all' ? s.btn : ''}>
+				All
+			</button>
+			<button
+				onClick={setHigh}
+				className={props.filter === 'high' ? s.btn : ''}
+			>
+				High
+			</button>
+			<button
+				onClick={setMiddle}
+				className={props.filter === 'middle' ? s.btn : ''}
+			>
+				Middle
+			</button>
+			<button onClick={setLow} className={props.filter === 'low' ? s.btn : ''}>
+				Low
+			</button>
+		</div>
+	);
 }
 
-export default Affairs
+export default Affairs;
